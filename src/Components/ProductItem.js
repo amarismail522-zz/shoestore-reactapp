8bc258ecbcd6cfa274b37fe84331d0a2fba0d4b9
amarismail522 <56680798+amarismail522@users.noreply.github.com>
2020-11-06 00:57:48 +0500

    Update ProductItem.js

--- src/Components/ProductItem.js
+++ src/Components/ProductItem.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import Shoes from '../store.json';
-import { Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { Container } from '@material-ui/core/';
-import ProductCard from './ProductCard';
 
 
 export default function ProductItem() {
